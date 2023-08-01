9a14fb78a36a545adacffa0e6940cdfe96ab7d74
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-08-01 17:57:07 +0900

    fix : builder patteren을 위해 @Builder 추가

--- src/main/java/com/example/sns_project/response/ResponseDto.java
+++ src/main/java/com/example/sns_project/response/ResponseDto.java
@@ -1,13 +1,20 @@
 package com.example.sns_project.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
 public class ResponseDto<T> {
     private final Integer code; // 1 성공, - 1 실패
     private final String message;
     private final T data;
+
+    @Builder
+    public ResponseDto(final Integer code, final String message, final T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
 }
 
