4aa4c0d046dc9e018e0dfd7a5da65b1892946221
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-08-01 19:07:27 +0900

    feat : 게시글 1개 조회

--- src/main/java/com/example/sns_project/service/PostService.java
+++ src/main/java/com/example/sns_project/service/PostService.java
@@ -1,7 +1,9 @@
 package com.example.sns_project.service;
 
+import com.example.sns_project.aop.ex.CustomApiException;
 import com.example.sns_project.domain.post.dto.PostCreate;
 import com.example.sns_project.domain.post.Post;
+import com.example.sns_project.domain.post.dto.PostOneResponse;
 import com.example.sns_project.domain.post.dto.PostResponseDto;
 import com.example.sns_project.repository.PostRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -18,8 +20,19 @@ public class PostService {
     }
 
     public PostResponseDto write(PostCreate postCreate) {
-        log.info("디버그 : service.writeaa : {}",postCreate);
         final Post post = postRepository.save(postCreate.toEntity());
         return new PostResponseDto(post.getTitle(), post.getContent());
     }
+
+    public PostOneResponse get(final Long postId) {
+        final Post post = postRepository.findById(postId).orElseThrow(
+                () -> new CustomApiException("존재하지 않는 글입니다.")
+        );
+
+        return PostOneResponse.builder()
+                .title(post.getTitle())
+                .content(post.getContent())
+                .build();
+    }
+
 }
