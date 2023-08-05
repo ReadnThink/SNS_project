988d0cbe0efeacb398fa77997fb6a4113ac73d45
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-08-05 15:44:41 +0900

    feat : 금칙어 기능 추가 (제목만 추가한 상태)
    금칙어 "바보"가 제목에 포함되어있을 시 예외발생

--- src/main/java/com/example/sns_project/exception/CustomApiException.java
+++ src/main/java/com/example/sns_project/exception/CustomApiException.java
@@ -1,9 +1,22 @@
 package com.example.sns_project.exception;
 
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
 public abstract class CustomApiException extends RuntimeException{
+
+    private final Map<String, String> validation = new HashMap<>();
+
     public CustomApiException(String message){
         super(message);
     }
 
     public abstract int statusCode();
+
+    public void addValidation(String fieldName, String message) {
+        validation.put(fieldName, message);
+    }
 }
