71482d5e922fc9e5798c8b78f21ef8125f492781
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-08-06 00:08:02 +0900

    refector : PostController BuilderPattern 도입 후 기존 생성자 패턴 주석 삭제
    Builder Pattern으로 변경 후 기존 주석처리했던 생성자 패턴 삭제

--- src/main/java/com/example/sns_project/controller/PostController.java
+++ src/main/java/com/example/sns_project/controller/PostController.java
@@ -28,10 +28,8 @@ public class PostController {
 
     @PostMapping("/posts")
     public ResponseEntity<?> post(@RequestBody @Valid PostCreate postCreate, BindingResult bindingResult) {
-        log.info("디버그 : {}", postCreate);
         postCreate.isValid();
         postService.write(postCreate);
-//        return new ResponseEntity<>(new ResponseDto<>(1, "글 작성을 성공했습니다.", null, null), OK);
         return new ResponseEntity<>(ResponseDto.builder()
                 .code(1)
                 .message("글 작성을 성공했습니다.")
@@ -41,7 +39,6 @@ public class PostController {
     @GetMapping("/posts/{postId}")
     public ResponseEntity<?> get(@PathVariable Long postId) {
         final PostResponse postResponse = postService.get(postId);
-//        return new ResponseEntity<>(new ResponseDto<>(1, "글 조회에 성공했습니다.", postResponse), OK);
         return new ResponseEntity<>(ResponseDto.builder()
                 .code(1)
                 .message("글 조회에 성공했습니다.")
@@ -52,7 +49,6 @@ public class PostController {
     @GetMapping("/posts")
     public ResponseEntity<?> getList(@ModelAttribute PostSearch postSearch) {
         final List<PostResponse> postList = postService.getList(postSearch);
-//        return new ResponseEntity<>(new ResponseDto<>(1, "글 리스트 조회를 성공했습니다.", postList), OK);
         return new ResponseEntity<>(ResponseDto.builder()
                 .code(1)
                 .message("글 리스트 조회를 성공했습니다.")
@@ -63,7 +59,6 @@ public class PostController {
     @PatchMapping("/posts/{postId}")
     public ResponseEntity<?> edit(@PathVariable Long postId, @RequestBody PostEdit postEdit) {
         postService.edit(postId, postEdit);
-//        return new ResponseEntity<>(new ResponseDto<>(1, "글 수정을 성공했습니다.", null), OK);
         return new ResponseEntity<>(ResponseDto.builder()
                 .code(1)
                 .message("글 수정을 성공했습니다.")
@@ -73,7 +68,6 @@ public class PostController {
     @DeleteMapping("/posts/{postId}")
     public ResponseEntity<?> delete(@PathVariable Long postId) {
         postService.delete(postId);
-//        return new ResponseEntity<>(new ResponseDto<>(1, "글 삭제를 성공했습니다.", null), OK);
         return new ResponseEntity<>(ResponseDto.builder()
                 .code(1)
                 .message("글 삭제를 성공했습니다.")
