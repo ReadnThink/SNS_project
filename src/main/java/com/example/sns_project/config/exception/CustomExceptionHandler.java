96aace4853f8489dc6b69808ba6992ae405d1c64
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-08-23 01:21:17 +0900

    refector : 예외처리 리펙토링
    - 커스텀 예외를 제외한 예외발생시 예외처리
    - RestControllerAdvice의 반환타입 변경 ? -> Response<Object>

--- src/main/java/com/example/sns_project/config/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/sns_project/config/exception/CustomExceptionHandler.java
@@ -10,16 +10,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 public class CustomExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseDto<Object>> allException(Exception e){
+        return new ResponseEntity<>(ResponseDto.builder()
+                .message("서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.")
+                .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     @ExceptionHandler(CustomApiException.class)
-    public ResponseEntity<?> apiException(CustomApiException e){
+    public ResponseEntity<ResponseDto<Object>> apiException(CustomApiException e){
         return new ResponseEntity<>(ResponseDto.builder()
                 .message(e.getMessage())
                 .build(), e.getStatus());
     }
 
     @ExceptionHandler(BindingException.class)
-    public ResponseEntity<?> validationApiException(BindingException e) {
+    public ResponseEntity<ResponseDto<Object>> validationApiException(BindingException e) {
         return new ResponseEntity<>(ResponseDto.builder()
                 .message(e.getMessage())
                 .build(), HttpStatus.BAD_REQUEST);
---
+++ src/main/java/com/example/sns_project/config/exception/InternalServerError.java
@@ -0,0 +1,18 @@
+package com.example.sns_project.config.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class InternalServerError extends CustomApiException {
+
+    private static final String MESSAGE = "서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.";
+
+    public InternalServerError() {
+        super(MESSAGE);
+    }
+
+    @Override
+    public HttpStatus getStatus() {
+        return HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+
+}
